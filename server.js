3929ccb46bb90a9bcdf8073a07ca5cb0dc725ef1
sejalkumari12-ui <sejalkumari5679@gmail.com>
2026-02-15 20:06:58 +0530

    database connected

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const app = require("./src/app")
-const mongoose = require("mongoose")
+const connectDB= require("./src/config/database")
 
-function connectDB (){
-    mongoose.connect("mongodb+srv://sejal:Seya7NgynqK1km4j@cluster0.lgrgo69.mongodb.net/curd operator")
-    .then (()=>{
-        console.log("data base is connected")
-    })
-}
+
 connectDB()
 
 app.listen(3000,()=>{
     console.log ("server running")
 })
+ 
---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+function connectDB (){
+    mongoose.connect("mongodb+srv://sejal:Seya7NgynqK1km4j@cluster0.lgrgo69.mongodb.net/curd operator")
+    .then (()=>{
+        console.log("data base is connected")
+    })
+}
+
+module.exports = connectDB
